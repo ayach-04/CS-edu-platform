e3050d2c5c002cf62c07ab8607b7476f321466c3
Aya <ilainjouri@gmail.com>
2025-04-29 00:36:30 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(cors({
     'http://localhost:5174',
     'https://edu-cs-platform.vercel.app',
     'https://cs-edu-platform-pl5o.vercel.app'
-  ], // Allow specific origins
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
